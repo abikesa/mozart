758f30db30d1ed7e8965af625533ca1fbc43775f
abikesa <abikesa.sh@gmail.com>
2024-09-27 17:38:45 -0400

    le nozze di figaro as preface

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,7 +1,9 @@
-# transfer after every jb clean .
-mkdir -p _build/html/pdfs  
-cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
-cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
-cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
-cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
+mkdir -p _build/html/pdfs _build/html/figures _build/html/media _build/html/data
+cp -r figures/* _build/html/figures/
+cp -r pdfs/* _build/html/pdfs/
+cp -r media/* _build/html/media/
 cp -r act1/app/* _build/html/act1/app
+cp -r data/* _build/html/data
+
+
+
